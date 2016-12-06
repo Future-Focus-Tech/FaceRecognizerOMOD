7f773b8c7e1b4d3100f28c320da49cfc92a38094
nsaikiran <saikiran.rguiiit@gmail.com>
2016-12-06 11:54:11 +0530

    [Sai Kiran,JP]| Echo controller with POST method

--- omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
+++ omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
@@ -3,14 +3,15 @@ package org.openmrs.module.facerecognizer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/rest/v1/facerec")
-public class FaceRecognizerController  {
-    @RequestMapping(value = "echo", method = RequestMethod.GET)
+public class FaceRecognizerController {
+    @RequestMapping(value = "echo", method = RequestMethod.POST)
     @ResponseBody
-    public String echo(){
-        return "Echo";
+    public String echo(@RequestParam("data") String data) {
+        return "You sent " + data;
     }
 }
