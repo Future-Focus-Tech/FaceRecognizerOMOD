4e02c202cf0729317eb7761b14a9f3d7b8154e7b
nsaikiran <saikiran.rguiiit@gmail.com>
2016-12-15 16:32:45 +0530

    [Sai Kiran]| Trying to load opencv core, for testing.

--- omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
+++ omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
@@ -1,17 +1,32 @@
 package org.openmrs.module.facerecognizer;
 
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import static org.bytedeco.javacpp.opencv_face.createLBPHFaceRecognizer;
+
+
 @Controller
 @RequestMapping(value = "/rest/v1/facerec")
 public class FaceRecognizerController {
     @RequestMapping(value = "echo", method = RequestMethod.POST)
     @ResponseBody
-    public String echo(@RequestParam("data") String data) {
-        return "You sent " + data;
+    public String echo(@RequestParam(value = "facesData") String[]
+                               facesData) {
+
+        int numberOfFaces = facesData.length;
+
+        Loader.load(org.bytedeco.javacpp.opencv_core.class);
+        FaceRecognizer lbphFaceRecognizer = createLBPHFaceRecognizer();
+        return String.valueOf(numberOfFaces);
+
     }
+
+
 }
