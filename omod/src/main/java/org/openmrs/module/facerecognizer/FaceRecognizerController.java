039d46fbc8140f05879a053e76057ee7bdf46d83
nsaikiran <saikiran.rguiiit@gmail.com>
2016-12-16 11:48:47 +0530

    [Sai Kiran]| Write the received image(s) recived from client to disk

--- omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
+++ omod/src/main/java/org/openmrs/module/facerecognizer/FaceRecognizerController.java
@@ -1,15 +1,32 @@
 package org.openmrs.module.facerecognizer;
 
 import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.MatVector;
+import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
 import static org.bytedeco.javacpp.opencv_face.createLBPHFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
 
 
 @Controller
@@ -18,7 +35,7 @@ public class FaceRecognizerController {
     @RequestMapping(value = "echo", method = RequestMethod.POST)
     @ResponseBody
     public String echo(@RequestParam(value = "facesData") String[]
-                               facesData) {
+                               facesData) throws IOException {
 
         int numberOfFaces = facesData.length;
 
@@ -26,9 +43,40 @@ public class FaceRecognizerController {
         FaceRecognizer lbphFaceRecognizer = createLBPHFaceRecognizer();
         FaceRecognizerWrapper wrapper = new FaceRecognizerWrapper
                 ("/opt/openmrs/facerec-files/learnedData.yml");
+
+        MatVector faces = new MatVector(numberOfFaces);
+
+        for (int faceIndex = 0; faceIndex < numberOfFaces; faceIndex++) {
+            BufferedImage bufferedImage = getBufferedImage(facesData[faceIndex]);
+            opencv_core.Mat matImage = createFaceMatrix(bufferedImage);
+            imwrite("/opt/openmrs/facerec-files/image" + faceIndex + ".png",
+                    matImage);
+            faces.put(faceIndex, matImage);
+        }
+
         return String.valueOf(numberOfFaces);
 
     }
 
+    private BufferedImage getBufferedImage(String imageDataUrl) throws IOException {
+        String encodingPrefix = "base64,";
+        int contentStartIndex = imageDataUrl.indexOf(encodingPrefix) + encodingPrefix.length();
+        byte[] imageData = DatatypeConverter.parseBase64Binary(imageDataUrl.substring(contentStartIndex));
+        InputStream inputStream = new ByteArrayInputStream(imageData);
+        return ImageIO.read(inputStream);
+
+    }
+
+    private Mat createFaceMatrix(BufferedImage bufferedImage) {
+        OpenCVFrameConverter.ToMat cv = new OpenCVFrameConverter.ToMat();
+        Java2DFrameConverter frameConverter = new Java2DFrameConverter();
+        Frame frame = frameConverter.getFrame(bufferedImage, 1.0, true);
+        opencv_core.IplImage iplImage = cv.convertToIplImage(frame);
+        int width = iplImage.width();
+        int height = iplImage.height();
+        IplImage grayIplImage = IplImage.create(width, height, IPL_DEPTH_8U, 1);
+        cvCvtColor(iplImage, grayIplImage, CV_BGR2GRAY);
+        return new Mat(grayIplImage);
+    }
 
 }
